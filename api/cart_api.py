5f043083094a48f6b872994276188993e3d64b61
LLC-9 <3064628554@qq.com>
2026-07-17 15:45:58 +0800

    Day 3:完成购物车接口，掌握接口关联和鉴权

---
+++ api/cart_api.py
@@ -0,0 +1,42 @@
+import requests
+
+
+#创建加入购物车的类
+class CarAPI:
+    def __init__(self):
+        self.base_url = "https://fakestoreapi.com"
+    def add_to_cart(self,token,user_id,product_id,quantity):
+        url = f"{self.base_url}/carts"
+        headers ={
+            "Authorization": f"Bearer {token}"
+        }
+        payload = {
+            "userID": user_id,
+            "date":"2026-07-17",
+            "products":[
+            {"productId":product_id,"quantity":quantity}
+        ]
+      }
+#发送请求接口返回数据
+        response = requests.post(url, json=payload, headers=headers,proxies={"http": None, "https": None})
+        return response.status_code,response.json()
+#测试
+if __name__ == '__main__':
+        from user_api import UserAPI
+        from product_api import ProductAPI
+
+        print(f"---测试---")
+        #调用登录测试接口，接收token并存储
+        user = UserAPI()
+        token = user.login("mor_2314","83r5^_")
+        print(f"密钥为:{token}")
+        #调用商品列表查看接口，接收商品id
+        product = ProductAPI()
+        t_product_id =  product.get_all_products()[2]["id"]
+        print(f"商品id为:{t_product_id}")
+        #调用测试加入购物车接口
+        cart = CarAPI()
+        status_codes, cart_result = cart.add_to_cart(token=token,user_id=5,product_id=t_product_id,quantity=1)
+
+        print(f"状态码为:{status_codes}\n,返回结果为：\n{cart_result}")
+
